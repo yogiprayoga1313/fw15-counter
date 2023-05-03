0eaaab62c2b533bc5d2291e07b8fc27006c1a0a7
Yogi Prayoga <yogiprayoga1333@gmail.com>
2023-05-03 15:00:23 +0700

    Daily task forntend 3/05/3023

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes:[
+      {
+        mytheme: {
+          "primary": "#570DF8",
+          "secondary": "#F000B8",
+          "accent": "#37CDBE",
+          "neutral": "#3D4451",
+          "base-100": "#FFFFFF",
+          "error": "#F87272",
+        }
+      }
+    ]
+  },
+  plugins: [
+    require("daisyui")
+  ],
+}
+
